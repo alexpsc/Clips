1531b44c20af3b4863e29f1d7e6b2354f5cf8551
AzureAD\AlexandruPascu <alexandru.pascu@mindit.io>
2022-11-09 12:19:23 +0200

    fix video play

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -17,21 +17,21 @@ import IClip from '../models/clip.model';
 })
 export class ClipComponent implements OnInit {
 
-  @ViewChild('videoPlayer', { static: true }) target?: ElementRef
-  player?: videojs.Player
+  // @ViewChild('videoPlayer', { static: true }) target?: ElementRef
+  // player?: videojs.Player
   clip!: IClip
   constructor(public route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.player = videojs(this.target?.nativeElement)
+    // this.player = videojs(this.target?.nativeElement)
 
     this.route.data.subscribe(data => {
       this.clip = data.clip as IClip
     })
-    this.player?.src({
-      src: this.clip.url,
-      type: "video/mp4"
-    })
+    // this.player?.src({
+    //   src: this.clip.url,
+    //   type: "video/mp4"
+    // })
 
   }
 
